cdd54905cfb4c6f5f6d75a386af714b70670aaa9
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-09-30 16:49:59 -0600

    Add test and passing code for method returning cards in given category

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,4 +9,9 @@ class Deck
     cards.size
   end
 
+  def cards_in_category(category)
+    cards.select do |card|
+      card.category == category
+    end
+  end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -40,4 +40,16 @@ class DeckTest < Minitest::Test
     assert_equal 4, deck.count
   end
 
+  def test_it_returns_array_of_cards_in_given_category
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
+    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
+    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
+    cards = [card1, card2, card3, card4]
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2], deck.cards_in_category(:Geography)
+    assert_equal [card3, card4], deck.cards_in_category(:SF_Trivia)
+  end
+  
 end
