b7c9b9918b4b093617bfe4554a78fea723eb192c
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-09-30 16:44:44 -0600

    Add test for counting method and passing code

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -16,4 +16,28 @@ class DeckTest < Minitest::Test
 
     assert_instance_of Deck, deck
   end
+
+  def test_it_contains_array_of_cards
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
+    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
+    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
+    cards = [card1, card2, card3, card4]
+    deck = Deck.new(cards)
+
+    assert_instance_of Card, deck.cards.first
+    assert_instance_of Array, deck.cards
+  end
+
+  def test_it_counts_number_of_cards
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
+    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
+    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
+    cards = [card1, card2, card3, card4]
+    deck = Deck.new(cards)
+
+    assert_equal 4, deck.count
+  end
+
 end
