aba9077977b697ac0c9a96bc36a410a34e7ea3d2
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-09-30 20:32:09 -0600

    Add tests and code for .deck and .turns methods

--- test/round_test.rb
+++ test/round_test.rb
@@ -18,4 +18,27 @@ class RoundTest < Minitest::Test
     assert_instance_of Round, round
   end
 
+  def test_deck_method_returns_deck
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
+    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
+    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+
+    assert_instance_of Deck, round.deck
+  end
+
+  def test_it_begins_with_empty_turns_array
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
+    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
+    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+    assert_empty round.turns
+  end
+
 end
