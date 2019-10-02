0263f7079317e601ec4c1c306d088f70ed3d5d29
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-02 15:18:15 -0600

    Refactor card generator to use File block so it automatically closes the file

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -10,11 +10,12 @@ class CardGenerator
 
   # Convert data in file into array of cards, returns that array
   def cards
-    # Instantiate File object
-    file = File.open(filename, "r")
-    file.readlines.map do |line|
-      card_elements = line.chomp.split(",")
-      Card.new(card_elements[0], card_elements[1], card_elements[2].to_sym)
+    # Open and iterate on file contents to create individual cards, closes automatically
+    File.open(filename, "r") do |file|
+      file.readlines.map do |line|
+        card_elements = line.chomp.split(",")
+        Card.new(card_elements[0], card_elements[1], card_elements[2].to_sym)
+      end
     end
   end
 end
