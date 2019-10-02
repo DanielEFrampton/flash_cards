0132209ec75ac4fda8bc4417828332bb69b900fd
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-01 21:23:14 -0600

    Incorporate CardGenerator into initialize method to pass Iteration 4 requirements

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,16 +2,14 @@ require './lib/turn'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
+require './lib/card_generator'
 
 class FlashcardRunner
   attr_reader :round
 
   def initialize
-    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
-    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
-    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
-    deck = Deck.new([card1, card2, card3, card4])
+    card_generator = CardGenerator.new("./lib/cards.txt")
+    deck = Deck.new(card_generator.cards)
     @round = Round.new(deck)
   end
 
