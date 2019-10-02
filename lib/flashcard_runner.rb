352803da5507a7c573c056d61cbd976893c3418e
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-10-01 20:14:28 -0600

    Add initial functioning flashcard_runner file

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+require './lib/turn'
+require './lib/card'
+require './lib/deck'
+require './lib/round'
+
+class FlashcardRunner
+  attr_reader :round
+
+  def initialize
+    card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card2 = Card.new("What is the capital of Indiana?", "Indianapolis", :Geography)
+    card3 = Card.new("What is the capital of Arrakis?", "Arrakeen", :SF_Trivia)
+    card4 = Card.new("What is the capital of Gondor?", "Minas Tirith", :SF_Trivia)
+    deck = Deck.new([card1, card2, card3, card4])
+    @round = Round.new(deck)
+  end
+
+  # Main CLI execution block
+  def start
+    # Introductory blurb
+    puts "Welcome! You're playing with #{round.deck.count} cards."
+    puts "-------------------------------------------------"
+    # Question-and-feedback loop
+    while (round.turns.size + 1) <= round.deck.count
+      puts "This is card number #{round.turns.size + 1} out of #{round.deck.count}."
+      puts "Question: #{round.current_card.question}"
+      round.take_turn(gets.chomp)
+      puts round.turns.last.feedback
+    end
+    # End of game report
+    puts "****** Game over! ******"
+    puts "You had #{round.number_correct} correct guesses out of #{round.deck.count} for a total score of #{round.percent_correct.round}."
+    # Loop to print percent_correct_by_category for each category present in deck
+    round.deck.cards.map {|card| card.category }.uniq.each do |category|
+      puts "#{category} - #{round.percent_correct_by_category(category).round}%"
+    end
+  end
+end
+
+# Running the runner
+game = FlashcardRunner.new
+game.start
